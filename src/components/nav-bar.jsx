c7d7b61d85b7168b4d71b1cf69158b48f5833c5c
Tarek-Sb <tksb06712@gmail.com>
2024-11-16 12:26:41 +0300

    home page ready on mobile

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -10,11 +10,16 @@ export default function NavBar () {
   return (
     <>
       <div className="w-full h-fit flex justify-between items-center px-6 py-4 border-white border-[3px] " >
-        <h1 className="w-fit h-fit text-white text-5xl font-medium select-none " >
+        <h1 className="w-fit h-fit text-white text-2xl  font-medium select-none " >
           Coffee Corner
         </h1>
+        <div>
+          <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.375 7.875H23.625M4.375 14H23.625M4.375 20.125H23.625" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </div>
         
-        <nav className="w-[50%] h-fit flex justify-between text-white text-2xl font-medium " >
+        <nav className="w-[50%] h-fit flex justify-between text-white text-base font-medium hidden " >
           <button onClick={() => setIsPressed("1")} className={`${isPressed === '1' ? 'underline underline-offset-8' : '' }`} >
             Home
           </button>
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -4,51 +4,52 @@ import NavBar from "../nav-bar";
 export default function HomePage() {
   return (
     <>
-      <div className="w-screen h-screen flex flex-col justify-between p-12 " >
-        <NavBar></NavBar>
-
-        <div className="w-full h-fit flex flex-col gap-2 " >
-          <h1 className="text-white text-[64px] leading-[1.5] font-semibold " >
-            Welcome to <br />
-            Coffee Corner café!
-          </h1>
-          <h2 className="text-white text-3xl font-light " >
-            were we believe in the power of a good cup of <br /> coffee and the joy of a freshly baked pastry.
-          </h2>
-          <h3 className="text-[#D1CFCD] text-base font-light" >
-            To never miss our upcoming offers and events.
-          </h3>
+      <div className="w-screen h-screen px-4 py-12 flex flex-col justify-between items-end " >
+        <div className="w-fit h-fit flex flex-col gap-12 " >
+          <NavBar></NavBar>
+          <div className="w-full h-fit flex flex-col gap-2 " >
+            <h1 className="text-white text-2xl font-semibold " >
+              Welcome to <br />
+              Coffee Corner café!
+            </h1>
+            <h2 className="text-white text-base font-light " >
+              were we believe in the power of a good cup of coffee and the joy of a freshly baked pastry.
+            </h2>
+            <h3 className="text-[#D1CFCD] text-xs font-light" >
+              To never miss our upcoming offers and events.
+            </h3>
+          </div>
         </div>
 
-        <div className="w-fit h-fit flex gap-24 items-center p-6 border-white border-[3px] " >
-          <div className="w-fit h-fit flex flex-col gap-4 items-center " >
-            <h1 className="text-white text-5xl " >
+        <div className="w-fit h-fit translate-x-4 " >
+          <img src="./cup.png" width={300} height={300} ></img>
+        </div>
+
+        <div className="w-full h-fit flex items-center justify-between p-2 border-white border-[3px] " >
+          <div className="w-fit h-fit flex flex-col gap-2 items-center " >
+            <h1 className="text-white text-2xl font-light " >
               50+
             </h1>
-            <h2 className="text-[#D1CFCD] text-2xl " >
+            <h2 className="text-[#D1CFCD] text-xs " >
               Menu items
             </h2>
           </div>
-          <div className="w-fit h-fit flex flex-col gap-4 items-center " >
-            <h1 className="text-white text-5xl " >
+          <div className="w-fit h-fit flex flex-col gap-2 items-center " >
+            <h1 className="text-white text-2xl font-light " >
               20+
             </h1>
-            <h2 className="text-[#D1CFCD] text-2xl " >
+            <h2 className="text-[#D1CFCD] text-xs " >
               Running orders
             </h2>
           </div>
-          <div className="w-fit h-fit flex flex-col gap-4 items-center " >
-            <h1 className="text-white text-5xl " >
+          <div className="w-fit h-fit flex flex-col gap-2 items-center " >
+            <h1 className="text-white text-2xl font-light " >
               2k+
             </h1>
-            <h2 className="text-[#D1CFCD] text-2xl " >
+            <h2 className="text-[#D1CFCD] text-xs " >
               Happy customers
             </h2>
           </div>
-        </div>
-
-        <div className="w-fit h-fit absolute right-0 top-40 " >
-          <img src="./cup.png" width={500} height={500} ></img>
         </div>
 
         <BackGround></BackGround>
