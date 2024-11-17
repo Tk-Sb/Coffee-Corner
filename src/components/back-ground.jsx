52b39e2e68afacdcc336f871c3d575a651398d89
Tarek-Sb <tksb06712@gmail.com>
2024-11-17 08:56:11 +0300

    test

--- src/components/back-ground.jsx
+++ src/components/back-ground.jsx
@@ -38,15 +38,17 @@ function BackGroundBlobs () {
         </div>
       </div>
 
-      <div className="w-fit h-fit hidden md:block absolute top-0 right-0 " >
-        <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.103279 0C160.62 328.067 400 223.16 400 223.16V400C180.221 377.228 -5.00693 92.8766 0.103279 0Z" fill="#261D14"/>
-        </svg>
-      </div>
-      <div className="w-fit h-fit hidden md:block absolute bottom-0 left-0 " >
-        <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M399.897 400C239.38 71.9326 0 176.84 0 176.84V0C219.779 22.7722 405.007 307.123 399.897 400Z" fill="#261D14"/>
-        </svg>
+      <div className="w-full h-full flex-col justify-between items-start hidden md:flex " >
+        <div className="w-full flex justify-end ">
+          <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.103279 0C160.62 328.067 400 223.16 400 223.16V400C180.221 377.228 -5.00693 92.8766 0.103279 0Z" fill="#261D14"/>
+          </svg>
+        </div>
+        <div className="w-full flex justify-start">
+          <svg width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M399.897 400C239.38 71.9326 0 176.84 0 176.84V0C219.779 22.7722 405.007 307.123 399.897 400Z" fill="#261D14"/>
+          </svg>
+        </div>
       </div>
     </>
   )
