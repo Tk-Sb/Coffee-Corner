d8d4c6b20abb19bd99ac2407d6a1a829b951331a
Tarek-Sb <tksb06712@gmail.com>
2024-11-24 08:27:39 +0300

    nav bar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
+import NavBar from "@/components/nav-bar";
 import HomeSection from "@/components/pages/home";
 
 export default function Home() {
   return (
     <>
+      <div className="w-full fixed px-4 md:px-6 lg:px-12 pt-12 z-50 ">
+        <NavBar className={`text-white`}></NavBar>
+      </div>
       <HomeSection></HomeSection>
     </>
   );
