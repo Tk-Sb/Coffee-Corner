03eef2a5345e5c25a52a0500f18e335b954229d3
Tarek-Sb <tksb06712@gmail.com>
2024-11-24 08:18:25 +0300

    tssssssssssst

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -2,19 +2,22 @@
 export default function Background ({ children }) {
   return (
     <>
-      <div className={`w-full h-svh px-4 md:px-6 lg:px-12 py-12 relative bg-[#1B1107] overflow-hidden overflow-y-scroll `} >
-        <svg className="absolute top-0 right-0 " width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <div className={`w-full h-fit px-4 md:px-6 lg:px-12 py-12 relative bg-[#1B1107] overflow-hidden `} >
+        <svg className="hidden md:block absolute top-0 right-0 " width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M0.103279 0C160.62 328.067 400 223.16 400 223.16V400C180.221 377.228 -5.00693 92.8766 0.103279 0Z" fill="#261D14"/>
         </svg>
-        <svg className="absolute bottom-0 left-0 " width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="hidden md:block absolute bottom-0 left-0 " width="400" height="400" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M399.897 400C239.38 71.9326 0 176.84 0 176.84V0C219.779 22.7722 405.007 307.123 399.897 400Z" fill="#261D14"/>
         </svg>
 
-        <div>
+        <svg className="md:hidden absolute top-0 right-0 " width="300" height="300" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.0774595 0C120.465 246.051 300 167.37 300 167.37V300C135.166 282.921 -3.7552 69.6574 0.0774595 0Z" fill="#261D14"/>
+        </svg>
+        <svg className="md:hidden absolute bottom-0 left-0 " width="300" height="300" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M299.923 300C179.535 53.9494 0 132.63 0 132.63V0C164.834 17.0791 303.755 230.343 299.923 300Z" fill="#261D14"/>
+        </svg>
 
-        </div>
-
-        <div className={`w-full fit flex flex-col gap-6 md:gap-12 relative `}>
+        <div className={`w-full h-full flex flex-col gap-4 relative `}>
           {children}
         </div>
       </div>
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -6,25 +6,25 @@ export default function HomeSection () {
   return (
     <>
       <Background>
-        <NavBar className={`text-white border-white `}></NavBar>
-        
-        <div className="flex flex-col gap-2 lg:gap-4 ">
-          <MainText className={`text-white `}>
-            Welcome to <br />
-            Coffee Corner café!
-          </MainText>
+        <div className={`w-full h-fit flex flex-wrap justify-end gap-4 md:gap-0  `}>
+          <div className="w-full h-fit xl:w-fit flex-grow flex flex-col gap-2 lg:gap-4  ">
+            <MainText className={`text-white `}>
+              Welcome to <br />
+              Coffee Corner café!
+            </MainText>
 
-          <SubText className={`text-white max-w-md md:max-w-2xl lg:max-w-4xl `}>
-            were we believe in the power of a good cup of coffee and the joy of a freshly baked pastry.
-          </SubText>
-          
-          <SmallText className={`text-[#D1CFCD] `}>
-            To never miss our upcoming offers and events.
-          </SmallText>
-        </div>
+            <SubText className={`text-white max-w-md md:max-w-2xl lg:max-w-4xl `}>
+              were we believe in the power of a good cup of coffee and the joy of a freshly baked pastry.
+            </SubText>
+            
+            <SmallText className={`text-[#D1CFCD] `}>
+              To never miss our upcoming offers and events.
+            </SmallText>
+          </div>
 
-        <div className={`w-full h-fit flex justify-end translate-x-4 md:translate-x-6 lg:translate-x-12  lg:absolute lg:top-32 lg:right-0 `}>
-          <img src="./cup.png" alt="coffee cup png" className={`w-[300px] h-[300px] md:w-[500px] md:h-[500px] `} />
+          <div className={`w-fit h-fit translate-x-4 md:translate-x-6 lg:translate-x-12 `}>
+            <img src="./cup.png" alt="coffee cup png" className={`w-[300px] h-[300px] md:w-[500px] md:h-[500px] `} />
+          </div>
         </div>
 
         <div className={`w-full h-fit lg:w-1/2 flex justify-between items-center px-2 md:px-6 py-3 lg:py-6 border-[3px] border-white text-white `} >
